6cc56434befa2fc10ba092ac7622b8750344e9bf
lenaen <lenaen@kth.se>
2016-02-08 21:35:52 +0100

    StartFrame som går att köra
    En del bortkommenterat, men går att köra för att se hur den kan se ut

--- StartFrame.java
+++ StartFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 //Vet ej vilka vi behöver förutom dessa
 
@@ -9,7 +10,7 @@ public class StartFrame extends JFrame implements ActionListener {
   //Fält:
 
     public boolean done;
-    private Chatprogram program;
+//    private Chatprogram program;
 
     //Grafiska komponenter:
     private JTextField nameField;
@@ -17,9 +18,9 @@ public class StartFrame extends JFrame implements ActionListener {
     private JButton serverButton;
 
     /** Konstruktor */
-    public StartFrame(ChatProgram mainProgram) {
+    public StartFrame(){//ChatProgram mainProgram) {
         //super();
-        program=mainprogram;
+        //program=mainprogram;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
   		BorderLayout startLayout = new BorderLayout();//Typ av layout can ändras
@@ -28,22 +29,35 @@ public class StartFrame extends JFrame implements ActionListener {
       //Lägger till textrad som lyssnas på
   		nameField = new JTextField(40);
   		nameField.addActionListener(this);
+      JLabel nameLabel= new JLabel("Name:");
 
       //Lägger till knappar
-
+      clientButton= new JButton("Client");
+      serverButton= new JButton("Server");
+      clientButton.addActionListener(this);
+      serverButton.addActionListener(this);
 
       //LÄgger in i grafisk layout
-  		add(nameField, BorderLayout.NORTH);//bara exempel på var ligger
+      JPanel namePanel=new JPanel();
+      namePanel.add(nameLabel);
+      namePanel.add(nameField);
+  		add(namePanel, BorderLayout.NORTH);//bara exempel på var ligger
+      JPanel buttonPanel=new JPanel();
+      add(buttonPanel, BorderLayout.SOUTH);
+      buttonPanel.add(clientButton);
+      buttonPanel.add(serverButton);
       pack();
       setVisible(true);
     }
 
-    private void setName(String nameText) {
-      program.name=nameText;
+    //setName kan inte användas som namn då det är en viktigt metod i JFrame
+    //som ej kan skrivas över
+    private void sendName(String nameText) {
+    //  program.name=nameText;
     }
 
     private void setType(Boolean type) {
-      program.isServer=type;
+    //  program.isServer=type;
     }
 
 
@@ -51,12 +65,23 @@ public class StartFrame extends JFrame implements ActionListener {
      för namn */
   	public void actionPerformed(ActionEvent e) {
   		if (e.getSource() == nameField) {
+        System.out.println("Skrev "+nameField.getText()+" i namnruta");//Ser i terminal att lyssnare funkar
 
   		} else if (e.getSource() == clientButton){
+        System.out.println("Klickade klient");
 
   		} else if (e.getSource() == serverButton){
-
+        System.out.println("Klickade server");
   		}
   	}
+
+    //För att testa hur fönstret blir.
+    //Kommenteras bort senare när del av program och tar då bort kommentar
+    //med chatprogram som variabel i konstruktorn
+
+    public static void main(String[] args) {
+      new StartFrame();
+
+    }
 
 }
